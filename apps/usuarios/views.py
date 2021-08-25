4a541681d395ad3346b4c85c1cc5df925a62ec60
Bruno de Souza Disliler <bruno_disliler@outlook.com>
2021-08-24 22:49:06 -0300

    Incremento - Paginação

--- apps/usuarios/views.py
+++ apps/usuarios/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from django.contrib.auth.models import User
 from django.contrib import auth, messages
 from receitas.models import Receita
+from django.contrib.auth.models import User
 
 
 def cadastro(request):
+    """ Cadastra uma nova pessoa no sistema """
     if request.method == 'POST':
         nome = request.POST['nome']
         email = request.POST['email']
@@ -34,6 +35,7 @@ def cadastro(request):
 
 
 def login(request):
+    """ Realiza login de pessoa no sistema """
     if request.method == 'POST':
         email = request.POST['email']
         senha = request.POST['senha']
@@ -45,7 +47,7 @@ def login(request):
             user = auth.authenticate(request, password=senha, username=nome)
             if user is not None:
                 auth.login(request, user)
-                print('login realizado com sucesso')
+                messages.success(request, 'login realizado com sucesso')
                 return redirect('dashboard')
     return render(request, 'usuarios/login.html')
 
@@ -65,33 +67,7 @@ def dashboard(request):
 def logout(request):
     auth.logout(request)
     return redirect('index')
-
-
-def cria_receita(request):
-    if request.method == 'POST':
-        nome_receita = request.POST['nome_receita']
-        ingredientes = request.POST['ingredientes']
-        modo_preparo = request.POST['modo_preparo']
-        tempo_preparo = request.POST['tempo_preparo']
-        rendimento = request.POST['rendimento']
-        categoria = request.POST['categoria']
-        foto_receita = request.FILES['foto_receita']
-        user = get_object_or_404(User, pk=request.user.id)
-        receita = Receita.objects.create(
-            pessoa=user, 
-            nome_receita=nome_receita,
-            ingredientes=ingredientes,
-            modo_preparo=modo_preparo,
-            tempo_preparo=tempo_preparo,
-            rendimento=rendimento,
-            categoria=categoria,
-            foto_receita=foto_receita,
-            )
-        receita.save()
-        return redirect('dashboard')
-    else:
-        return render(request, 'usuarios/cria_receita.html')
-
+            
 
 def campo_vazio(campo):
     return not campo.strip()
